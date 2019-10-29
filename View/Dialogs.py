72ef59891f5e406edd1a183422342eeeb703726d
andrei <andreimd02@gmail.com>
2019-10-29 19:31:58 -0300

    refatoração das classes e adição de funcionalidades

---
+++ View/Dialogs.py
@@ -0,0 +1,83 @@
+from PyQt5.QtWidgets import QDialog, QLineEdit, QDialogButtonBox, QFormLayout, QLabel, QRadioButton, QGridLayout
+
+class Dialogs(QDialog):
+        def __init__(self, parent=None):
+            super().__init__(parent)
+
+        def detectorNumbers(self):
+            self.setWindowTitle("Choose the number of detectors")
+
+            self.numbers = QLineEdit(self)
+            self.size = QLineEdit(self)
+            buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+
+            layout = QFormLayout(self)
+            layout.addRow("Number of Detectors", self.numbers)
+            layout.addRow("Size of each Detector", self.size)
+            layout.addWidget(buttonBox)
+
+            buttonBox.accepted.connect(self.accept)
+            buttonBox.rejected.connect(self.reject)
+
+        def getDetInputs(self):
+            return (int(self.numbers.text()), int(self.size.text()))
+
+        def actorTrajectory(self, proj_numbers):
+            self.setWindowTitle("Choose the Trajectory configuration")
+
+            buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+
+
+            label_projections = QLabel("Number of Projections:")
+            self.projections = QLineEdit(self)
+
+            label_trajectory = QLabel("Trajectory Type: ")
+            self.circle_button = QRadioButton("Circle Trajectory")
+            self.straight_button = QRadioButton("Straight Trajectory")
+            self.custom_button = QRadioButton("Custom Trajectory")
+
+            if (proj_numbers != None):
+                self.projections.setText(str(proj_numbers))
+
+            grid_layout = QGridLayout()
+            grid_layout.addWidget(label_projections, 1, 0)
+            grid_layout.addWidget(self.projections, 1, 1)
+            grid_layout.addWidget(label_trajectory, 2, 0)
+            grid_layout.addWidget(self.circle_button, 2, 1)
+            grid_layout.addWidget(self.straight_button, 3, 1)
+            grid_layout.addWidget(self.custom_button, 4, 1)
+            grid_layout.addWidget(buttonBox, 5, 1)
+
+            self.setLayout(grid_layout)
+
+            buttonBox.accepted.connect(self.accept)
+            buttonBox.rejected.connect(self.reject)
+
+        def getTrajInputs(self):
+            if(self.circle_button.isChecked()):
+                return (int(self.projections.text()), "circle_trajectory")
+            elif(self.straight_button.isChecked()):
+                return (int(self.projections.text()), "straight_trajectory")
+            elif(self.custom_button.isChecked()):
+                return (int(self.projections.text()), "custom_trajectory")
+            # else:
+            #     return error
+
+        def xyPosition(self):
+            self.setWindowTitle("Choose the coordinates")
+
+            self.x = QLineEdit(self)
+            self.y = QLineEdit(self)
+            buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+
+            layout = QFormLayout(self)
+            layout.addRow("X Axis", self.x)
+            layout.addRow("Y axis", self.y)
+            layout.addWidget(buttonBox)
+
+            buttonBox.accepted.connect(self.accept)
+            buttonBox.rejected.connect(self.reject)
+
+
+        def getxyInputs(self):
+            return (int(self.x.text()), int(self.y.text()))
--- interactor2d.py
+++ interactor2d.py
@@ -1,51 +1,64 @@
 import vtk
+import math
 
-class CustomInteractor(vtk.vtkInteractorStyleTrackballCamera):
+class CustomInteractor(vtk.vtkInteractorStyleTrackballActor):
 
     def __init__(self, renderer, renWin):
+        super().__init__()
         self.AddObserver('LeftButtonPressEvent', self.OnLeftButtonDown)
         self.AddObserver('LeftButtonReleaseEvent', self.OnLeftButtonRelease)
         self.AddObserver('MouseMoveEvent', self.OnMouseMove)
+        self.AddObserver('RightButtonPressEvent', self.OnRightButtonDown)
+        self.AddObserver('RightButtonReleaseEvent', self.OnRightButtonRelease)
+        self.AddObserver('MiddleButtonPressEvent', self.OnMiddleButtonDown)
+        self.AddObserver('MiddleButtonReleaseEvent', self.OnMiddleButtonRelease)
+        self.rotation = False
 
         self.renderer = renderer
         self.chosenPiece = None
         self.renWin = renWin
 
-    def OnRightButtonDown(self):
+    def OnRightButtonDown(self, obj, eventType):
         clickPos = self.GetInteractor().GetEventPosition()
 
         picker = vtk.vtkPropPicker()
         picker.Pick(clickPos[0], clickPos[1], 0, self.renderer)
-        actor = picker.GetActor2D()
+        actor = picker.GetActor()
         self.chosenPiece = actor
-        vtk.vtkInteractorStyleTrackballCamera.OnRightButtonDown(self)
 
-    def OnRightButtonRelease(self):
+    def OnRightButtonRelease(self, obj, eventType):
         self.chosenPiece = None
-        vtk.vtkInteractorStyleTrackballCamera.OnRightButtonUp(self)
 
     def OnLeftButtonRelease(self, obj, eventType):
-        print(self.chosenPiece.GetPosition())
         self.chosenPiece = None
-        vtk.vtkInteractorStyleTrackballCamera.OnLeftButtonUp(self)
+        vtk.vtkInteractorStyleTrackballActor.OnMiddleButtonUp(self)
 
     def OnLeftButtonDown(self, obj, eventType):
+        vtk.vtkInteractorStyleTrackballActor.OnMiddleButtonDown(self)
+
+    def OnMiddleButtonDown(self, obj, eventType):
         clickPos = self.GetInteractor().GetEventPosition()
 
         picker = vtk.vtkPropPicker()
         picker.Pick(clickPos[0], clickPos[1], 0, self.renderer)
-        actor = picker.GetActor2D()
+        actor = picker.GetActor()
         self.chosenPiece = actor
-        vtk.vtkInteractorStyleTrackballCamera.OnLeftButtonDown(self)
+        self.rotation = True
+
+    def OnMiddleButtonRelease(self, obj, eventType):
+        self.chosenPiece = None
 
     def OnMouseMove(self, obj, eventType):
-        if self.chosenPiece is not None:
+        if self.rotation:
+            if self.chosenPiece is not None:
+                mouse_pos_pas = self.GetInteractor().GetLastEventPosition()
+                mouse_pos = self.GetInteractor().GetEventPosition()
+                rotate_x = mouse_pos[0] - mouse_pos_pas[0]
 
-            mousePos = self.GetInteractor().GetEventPosition()
+                self.chosenPiece.RotateZ(rotate_x)
+                self.renWin.Render()
 
-            self.chosenPiece.SetPosition(mousePos[0], mousePos[1])
-
-            self.renWin.Render()
-        else :
-            vtk.vtkInteractorStyleTrackballCamera.OnMouseMove(self)
-
+            else:
+                vtk.vtkInteractorStyleTrackballActor.OnMouseMove(self)
+        else:
+            vtk.vtkInteractorStyleTrackballActor.OnMouseMove(self)
